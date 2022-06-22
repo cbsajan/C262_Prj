fe6a9dd1448467fdd0411fb6cc5da0cf17c03133
C B Sajan <sajan@ckhathri.com>
2022-06-22 19:45:03 +0530

    Updated App.py

--- app.py
+++ app.py
@@ -8,23 +8,6 @@ app = Flask(__name__)
 # route your webpage
 
 
-@app.route('/')
-def visitors():
-
-    # Load current count
-    counter_read_file = open("count.txt", "r")
-    visitors_count = int(counter_read_file.read())
-    counter_read_file.close()
-
-    # Increment the count
-    visitors_count = visitors_count + 1
-
-    # Overwrite the count
-    counter_write_file = open("count.txt", "w")
-    counter_write_file.write(str(visitors_count))
-    counter_write_file.close()
-    return render_template("index.html", count=visitors_count)
-
 # Render HTML with count variable
 
 # route your webpage
@@ -32,10 +15,6 @@ def visitors():
 
 @app.route('/', methods=['POST'])
 def covid_stats():
-    # # Load current count
-    # counter_read_file = open("count.txt", "r")
-    # visitors_count = int(counter_read_file.read())
-    # counter_read_file.close()
 
     selected_option = request.form['operation']
     expression = request.form['expression']
